ef19c6a20a19db5e17a7852eb86af176e9326f55
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2022-10-24 14:48:22 -0500

    ....

--- front/src/App.js
+++ front/src/App.js
@@ -6,19 +6,10 @@ import { Footer } from './components/layout/Footer';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      <center>Contenido aqui</center>
-      
-      <Footer />
-      
-      </div>
+        <Header />
+        
+        <Footer />
+    </div>
   );
 }
 
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import "../../App.css"
 
 const Header = () => {
 
