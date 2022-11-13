1f7c8a0c13b264d694a59ec080def130643a40df
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2022-11-13 14:23:23 -0500

    Clase 23 Parcial

--- front/src/actions/userActions.js
+++ front/src/actions/userActions.js
@@ -4,7 +4,10 @@ import{
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS, 
+    REGISTER_USER_FAIL
 } from "../constants/userConstants"
 
 //Login
@@ -18,7 +21,7 @@ export const login = (email, password) => async (dispatch) =>{
             }
         }
 
-        const {data} = await axios.get('/api/login', {email, password}, config)
+        const {data} = await axios.post('/api/login', {email, password}, config)
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -39,4 +42,31 @@ export const clearErrors= () => async (dispatch) =>{
     dispatch({
         type: CLEAR_ERRORS
     })
+}
+
+//REGISTRAR USUARIO
+export const register = (userData) => async (dispatch) =>{
+    try{
+        dispatch({type: REGISTER_USER_REQUEST})
+
+        const config={
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const {data} = await axios.post('/api/usuario/registro',userData, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+    }
+    catch (error) {
+        dispatch({
+            type:REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+
+    }
 }
