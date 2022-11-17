fdadef41b3e5dce63b751afa6d7c4a3c6590ee1e
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2022-11-16 19:49:10 -0500

    Reviews (Controllers and routes)

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -144,16 +144,16 @@ exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
 exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
     const product = await producto.findById(req.query.idProducto);
 
-    const opi = product.opiniones.filter(opinion =>
+    const opiniones = product.opiniones.filter(opinion =>
         opinion._id.toString() !== req.query.idReview.toString());
 
-    const numCalificaciones = opi.length;
+    const numCalificaciones = opiniones.length;
 
-    const calificacion = opi.reduce((acc, Opinion) =>
-        Opinion.rating + acc, 0) / opi.length;
+    const calificacion = opiniones.reduce((acc, Opinion) =>
+        Opinion.rating + acc, 0) / opiniones.length;
 
     await producto.findByIdAndUpdate(req.query.idProducto, {
-        opi,
+        opiniones,
         calificacion,
         numCalificaciones
     }, {
